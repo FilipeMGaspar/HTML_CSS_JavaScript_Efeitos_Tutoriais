332f2a69952a7215be1f4d86f32c661242d1a713
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-05 18:54:08 +0100

    Animated Profile Card Using HTML CSS AND JAVASCRIPT - Programmer Cloud

--- Animated_Profile_Card-Programmer-Cloud/js/script.js
+++ Animated_Profile_Card-Programmer-Cloud/js/script.js
@@ -1,2 +1,13 @@
 const buttons = document.querySelectorAll(".card-buttons button")
 const sections = document.querySelectorAll(".card-section")
+const card = document.querySelectorAll(".card")
+
+const handleButtonClick = e => {
+    const targetSection = e.target.getAttribute("data-section")
+    const section = document.querySelector(targetSection)
+    targetSection !== "#about" ?
+    card.classList.add("is-active") :
+    card.classList.remove("is-active")
+    card.setAttribute("data-state", targetSection)
+    
+}

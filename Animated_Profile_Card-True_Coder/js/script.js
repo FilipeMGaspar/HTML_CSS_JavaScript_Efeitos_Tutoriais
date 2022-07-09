1accf87b06b0c559befb580e11174884c6d003e6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-09 11:30:21 +0100

    Animated Profile Card Using HTML & CSS & JavaScript - True Coder

--- Animated_Profile_Card-True_Coder/js/script.js
+++ Animated_Profile_Card-True_Coder/js/script.js
@@ -1,8 +1,10 @@
 const image = document.querySelector('.image')
 const hover = document.querySelector('.hover')
+const modal = document.querySelector('.modal')
 
 function show() {
-    
+    hover.classList.add('active');
+    modal.classList.add('show');
 }
 
 image.addEventListener('click', show)

3cca8464538b38b0aff6eb498938a41c5ce086f2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-29 14:55:01 +0100

    Animated Login and Registration Form in HTML CSS & JavaScript - CodingNepal

--- Animated_Login_and_Registration_Form-CodingNepal/js/script.js
+++ Animated_Login_and_Registration_Form-CodingNepal/js/script.js
@@ -4,6 +4,8 @@ const signupForm = document.querySelector("form.signup");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 
+const signupLink = document.querySelector(".signup-link a");
+
 signupBtn.onclick = (() => {
     loginForm.style.marginLeft = "-50%";
 });

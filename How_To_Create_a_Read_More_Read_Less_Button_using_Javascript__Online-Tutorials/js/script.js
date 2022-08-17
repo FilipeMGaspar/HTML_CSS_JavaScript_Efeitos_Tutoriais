18c1b58e4429c40819d8bc9a8d59e7fbebf33532
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-08-17 20:18:51 +0100

    How To Create a Read More Read Less Button using Javascript - Online Tutorials

--- How_To_Create_a_Read_More_Read_Less_Button_using_Javascript__Online-Tutorials/js/script.js
+++ How_To_Create_a_Read_More_Read_Less_Button_using_Javascript__Online-Tutorials/js/script.js
@@ -2,6 +2,6 @@ let more = document.querySelectorAll(".more");
 
 for (let i = 0 ; i < more.length; i++) {
     more[i].addEventListener("click", function(){
-        more[i].parentNode.classList.add('active')
+        more[i].parentNode.classList.toggle('active')
     })
 }

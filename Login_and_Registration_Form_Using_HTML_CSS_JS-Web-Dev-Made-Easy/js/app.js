66fe78f54322b87f44d72537c9388ad3a61bd885
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-09-06 12:44:55 +0100

    Login and Registration Form Using HTML CSS JS - Web Dev Made Easy

--- Login_and_Registration_Form_Using_HTML_CSS_JS-Web-Dev-Made-Easy/js/app.js
+++ Login_and_Registration_Form_Using_HTML_CSS_JS-Web-Dev-Made-Easy/js/app.js
@@ -1,2 +1,8 @@
 const sign_in_btn = document.querySelector("#sign-in-button")
 const sign_up_btn = document.querySelector("#sign-up-button")
+const container = document.querySelector(".container")
+
+
+sign_up_btn.addEventListener("click", ()=>{
+    container.classList.add("sign-up-mode")
+})

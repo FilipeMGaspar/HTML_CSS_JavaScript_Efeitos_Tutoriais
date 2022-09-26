78853752568699ea67340414fc0c5ea770605ca9
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-09-26 11:29:26 +0100

    Projeto Calculadora de IMC com JavaScript - Matheus Battisti - Hora de Codar

--- Projeto_Calculadora_de_IMC_com_JavaScript-Matheus-Battisti-Hora-de-Codar/js/script.js
+++ Projeto_Calculadora_de_IMC_com_JavaScript-Matheus-Battisti-Hora-de-Codar/js/script.js
@@ -47,7 +47,16 @@ const calcBtn = document.querySelector("#calc-btn");
 const clearBtn = document.querySelector("#clear-btn");
 
 // Funções
+function createTable(data) {
+    data.forEach((item) => {
+        const div = document.createElement("div");
+        div.classList.add("table-data");
 
+
+        const classification = document.createElement("p");
+        classification.innerText = intem.classification;
+    });
+}
 
 // Inicialização
 
